506458e413e11d3fdec3ebc144c4201788e57586
Masha Ikromova <mashikro@gmail.com>
2020-04-11 18:08:14 -0400

    Added a while true loop so client can recieve a stream of data

--- client.py
+++ client.py
@@ -7,12 +7,20 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket object
 
 s.connect(('0.0.0.0', 1234)) #same port as the server
 
-# To recieve the message from the server:
-# recv() takes in 1 param - recv_size - the size/chunk of data you are permitting to recieve at a time
-# recv_size of 1024 bytes is big enough for what we are doing. If we were sending massive files, we would need something bigger
+full_message = "" 
+# keep recieving streams of data forever
+while True:
+    # To recieve the message from the server:
+    # recv() takes in 1 param - recv_size - the size/chunk of data you are permitting to recieve at a time
+    # recv_size of 1024 bytes is big enough for what we are doing. If we were sending massive files, we would need something bigger
 
-message = s.recv(1024) # in bytes
+    message = s.recv(14) # in bytes
 
-# we recieve bytes and then we decode them
-# decode() takes in char encoding format 
-print(message.decode('utf-8'))
+    if not message:
+        break
+
+    # we recieve bytes and then we decode them
+    # decode() takes in char encoding format 
+    full_message += message.decode('utf-8') 
+
+print(full_message)
